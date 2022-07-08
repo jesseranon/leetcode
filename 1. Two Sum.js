94731599f501e5509c7f0a4bb14f5df77110ac1c
Jetsuko <jessean.infiltrator@gmail.com>
2022-07-08 15:57:37 -0700

    Accepted 202 ms | 41.8 MB

--- 1. Two Sum.js
+++ 1. Two Sum.js
@@ -4,18 +4,11 @@
  * @return {number[]}
  */
  var twoSum = function(nums, target) {
-    const res = [];
     for (let i = 0; i < nums.length - 1; i++) {
-        if (res.length !== 2) {
-            const first = nums[i];
-            for (let j = i + 1; j < nums.length; j++) {
-                if (first + nums[j] === target) {
-                    res.push(i);
-                    res.push(j)
-                    break;
-                }
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
             }
-        } else break;  
+        }
     }
-    return res;
 };
